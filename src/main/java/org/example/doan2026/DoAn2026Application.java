01cb4f41a29ee2df2efd41ea73cb54af850f9cb4
Cường Nguyễn Đức <chuoidochoduoi7e@gmail.com>
2026-05-17 13:29:02 +0700

    tao du an

---
+++ src/main/java/org/example/doan2026/DoAn2026Application.java
@@ -0,0 +1,13 @@
+package org.example.doan2026;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoAn2026Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DoAn2026Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/doan2026/DoAn2026ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.doan2026;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoAn2026ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
